824d1f69958a4a924b31578c2d4c4e9404644b88
bald03 <daniil.bariev@mail.ru>
2023-05-05 00:07:35 +0300

    'Lab6'

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type indexPage struct {
@@ -18,116 +20,106 @@ type postPage struct {
 }
 
 type featuredPostData struct {
-	Title       string
-	Subtitle    string
-	ImgModifier string
-	Author      string
-	AuthorImg   string
-	PublishDate string
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	ImgModifier string `db:"image_url"`
+	Author      string `db:"author"`
+	AuthorImg   string `db:"author_url"`
+	PublishDate string `db:"publish_date"`
 }
 
 type mostRecentPostData struct {
-	Title       string
-	Subtitle    string
-	ImgModifier string
-	Author      string
-	AuthorImg   string
-	PublishDate string
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	ImgModifier string `db:"image_url"`
+	Author      string `db:"author"`
+	AuthorImg   string `db:"author_url"`
+	PublishDate string `db:"publish_date"`
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-		return                                      // Не забываем завершить выполнение ф-ии
-	}
-	data := indexPage{
-		Title:          "Escape",
-		FeaturedPosts:  featuredPosts(),
-		MostRecentPost: mostRecentPost(),
-	}
+func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		featuredPosts, err := featuredPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err)
+			return // Не забываем завершить выполнение ф-ии
+		}
+		mostRecentPost, err := mostRecentPost(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err)
+			return // Не забываем завершить выполнение ф-ии
+		}
 
-	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
-	}
-}
+		ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return                                      // Не забываем завершить выполнение ф-ии
+		}
 
-func featuredPosts() []featuredPostData {
-	return []featuredPostData{
-		{
-			Title:       "The Road Ahead",
-			Subtitle:    "The road ahead might be paved - it might not be.",
-			ImgModifier: "static/img/articles_img/the-road-ahead-post.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "The Road Ahead",
-			Subtitle:    "The road ahead might be paved - it might not be.",
-			ImgModifier: "static/img/articles_img/from-top-down.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
+		data := indexPage{
+			Title:          "Escape",
+			FeaturedPosts:  featuredPosts,
+			MostRecentPost: mostRecentPost,
+		}
+
+		err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
 	}
 }
 
-func mostRecentPost() []mostRecentPostData {
-	return []mostRecentPostData{
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgModifier: "static/img/articles_img/still_standing_tall_img.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/autor_avatars/william_wong.jpg",
-			PublishDate: "September 25, 2015",
-		},
+func featuredPosts(db *sqlx.DB) ([]featuredPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			image_url,
+			author,
+			author_url,
+			publish_date
+		FROM
+			post
+		WHERE featured = 1
+	` // Составляем SQL-запрос для получения записей для секции featured-posts
+
+	var posts []featuredPostData // Заранее объявляем массив с результирующей информацией
+
+	err := db.Select(&posts, query) // Делаем запрос в базу данных
+	if err != nil {                 // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
 	}
+
+	return posts, nil
+}
+
+func mostRecentPost(db *sqlx.DB) ([]mostRecentPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			image_url,
+			author,
+			author_url,
+			publish_date
+		FROM
+			post
+		WHERE featured = 0
+	` // Составляем SQL-запрос для получения записей для секции featured-posts
+
+	var posts []mostRecentPostData // Заранее объявляем массив с результирующей информацией
+
+	err := db.Select(&posts, query) // Делаем запрос в базу данных
+	if err != nil {                 // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
+	}
+
+	return posts, nil
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
---
+++ data/sql/posts/posts.sql
@@ -0,0 +1,15 @@
+INSERT INTO post (title, subtitle, author, author_url, publish_date, image_url, featured) VALUES ('The Road Ahead', 'The road ahead might be paved - it might not be.', 'Mat Vogels', 'static/img/autor_avatars/mat_vogels.jpg', 'September 25, 2015', 'static/img/articles_img/the-road-ahead-post.png', 1);
+
+INSERT INTO post (title, subtitle, author, author_url, publish_date, image_url, featured) VALUES ('From Top Down', 'Once a year, go someplace you’ve never been before.', 'William Wong', 'static/img/autor_avatars/mat_vogels.jpg', 'September 25, 2015', 'static/img/articles_img/from-top-down.png', 1);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Still Standing Tall', 'Life begins at the end of your comfort zone', 'static/img/articles_img/still_standing_tall_img.jpg', 'William Wong', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Sunny Side Up', 'No place is ever as bad as they tell you it’s going to be.', 'static/img/articles_img/sunny_side_up_img.png', 'Mat Vogels', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Water Falls', 'We travel not to escape life, but for life not to escape us.', 'static/img/articles_img/water_falls_img.png', 'Mat Vogels', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Through the Mist', 'Travel makes you see what a tiny place you occupy in the world.', 'static/img/articles_img/through_the_mist_img.png', 'William Wong', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Awaken Early', 'Not all those who wander are lost.', 'static/img/articles_img/awaken_early_img.png', 'Mat Vogels', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Try it Always', 'The world is a book, and those who do not travel read only one page.', 'static/img/articles_img/try_it_always_img.png', 'Mat Vogels', 'static/img/autor_avatars/mat_vogels.jpg', '9/25/2015',  0);
