1ac2caa45420c1ebe22808b9fc2564ae782c5fd2
bald03 <daniil.bariev@mail.ru>
2023-05-04 22:45:20 +0300

    'Lab6'

---
+++ data/sql/table/posts.sql
@@ -0,0 +1,13 @@
+INSERT INTO post (title, subtitle, author, author_url, publish_date, image_url, featured) VALUES ('The Road Ahead', 'The road ahead might be paved - it might not be.', 'Mat Vogels', 'static/img/post_autor/mat_vogels.jpg', 'September 25, 2015', 'static/img/post_photo/northern.png', 1);
+
+INSERT INTO post (title, subtitle, author, author_url, publish_date, image_url, featured) VALUES ('From Top Down', 'Once a year, go someplace you’ve never been before.', 'William Wong', 'static/img/post_autor/william_wong.jpg', 'September 25, 2015', 'static/img/post_photo/fireball.png', 1);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Still Standing Tall', 'Life begins at the end of your comfort zone', 'static/img/post_photo/balls.jpg', 'William Wong', 'static/img/post_autor/william_wong.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Sunny Side Up', 'No place is ever as bad as they tell you it’s going to be.', 'static/img/post_photo/bridge.png', 'Mat Vogels', 'static/img/post_autor/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Water Falls', 'We travel not to escape life, but for life not to escape us.', 'static/img/post_photo/lake.png', 'Mat Vogels', 'static/img/post_autor/mat_vogels.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Through the Mist', 'Travel makes you see what a tiny place you occupy in the world.', 'static/img/post_photo/ocean.png', 'William Wong', 'static/img/post_autor/william_wong.jpg', '9/25/2015',  0);
+
+INSERT INTO post (title, subtitle, image_url,author, author_url, publish_date,  featured) VALUES ('Awaken Early', 'Not all those who wander are lost.', 'static/img/post_photo/fog.png', 'Mat Vogels', 'static/img/post_autor/mat_vogels.jpg', '9/25/2015',  0);
